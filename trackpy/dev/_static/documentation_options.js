c4f82c735e38f7ecd2104884d3b7759d664718da
Nathan Keim <keim@psu.edu>
2024-07-10 16:00:04 -0400

    Development docs for trackpy v0.6.4

--- trackpy/dev/_static/documentation_options.js
+++ trackpy/dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.3',
+    VERSION: '0.6.3+2.ga65a2a6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
